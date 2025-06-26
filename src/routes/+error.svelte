b5ee4ed25269dc8de26fc39ab95efe3e6fafd74b
Jaasim2008 <jaasim2008@gmail.com>
2025-06-26 23:01:03 +0400

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/state';
+    export let error;
+</script>
+
+<div id="centered">
+    <h1>{page.status} {page.error.message} 😞</h1>
+    <i>you may create a issue at the <a href="https://github.com/Jaasim2008/examfilesv3/issues" target="_blank">github repo</a></i>
+</div>
+
+<style>
+    #centered {
+        text-align: center;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+    #centered > i {
+        font-size: x-small;
+    }
+    #centered > i > a {
+        color: blue;
+        text-decoration-line: underline;
+    }
+</style>
